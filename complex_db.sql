c796a92d694d7071ab9359e3fe247d5f015840f7
iz.aia.h <imukisa024@gmail.com>
2023-02-11 08:02:27 +0300

    starting the company db

--- complex_db.sql
+++ complex_db.sql
@@ -1,12 +1,29 @@
--- Active: 1676014376010@@127.0.0.1@3306
+-- Active: 1676029288948@@localhost@3306
+CREATE DATABASE Company_db
+    DEFAULT CHARACTER SET = 'utf8mb4';
+
+USE Company_db;
 
 CREATE TABLE employee (
-    emp_id INT PRIMARY KEY,
-    first_name VARCHAR(40),
-    last_name VARCHAR(40),
-    birth_day DATE,
-    sex VARCHAR(1),
-    salary INT,
-    super_id INT,
-    branch_id INT
+  emp_id INT PRIMARY KEY,
+  first_name VARCHAR(40),
+  last_name VARCHAR(40),
+  birth_day DATE,
+  sex VARCHAR(1),
+  salary INT,
+  super_id INT,
+  branch_id INT
 );
+
+CREATE TABLE branch (
+  branch_id INT PRIMARY KEY,
+  branch_name VARCHAR(40),
+  mgr_id INT,
+  mgr_start_date DATE,
+  FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
+);
+
+-- mgr_id stands for manager_id
+
+DESCRIBE employee;
+DESCRIBE branch;
