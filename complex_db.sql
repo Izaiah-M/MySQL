eda6423ec647e2a94aac24843e3d95cb7f781633
iz.aia.h <imukisa024@gmail.com>
2023-02-16 10:19:14 +0300

    learning joins

--- complex_db.sql
+++ complex_db.sql
@@ -216,5 +216,10 @@ SELECT branch_name
 FROM  branch;  --returns all the info in one column.
 
 -- USING JOINS
+INSERT INTO branch VALUES(4, 'Buffalo', NULL, NULL);
 
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+JOIN branch
+ON employee.emp_id = branch.mgr_id;
 
